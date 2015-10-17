6b8755a4229da7918827c646cdd7297e4350ac07
felipe <fe.valdiviaa@gmail.com>
2015-10-17 01:58:18 -0300

    Add facebook credentials

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider  :facebook,
-            ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
+            ENV['1493795334248559'], ENV['853eac761c67c522b0e0de5d26da0ec9'],
             scope: 'public_profile', display: 'page', image_size: 'square'
 end
