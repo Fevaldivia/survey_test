c65634c7835cb04b4311e9677dd2fac5cef9e201
felipe <fe.valdiviaa@gmail.com>
2015-10-17 00:52:55 -0300

    Add methods to questions controller for edit, destroy and update

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -17,9 +17,33 @@ class QuestionsController < ApplicationController
     end
   end
 
+  def edit
+  @question = Question.find_by_id(params[:id])
+  end
+
+  def update
+    @question = Question.find_by_id(params[:id])
+    if @question.update_attributes(question_params)
+      flash[:success] = 'Question was updated!'
+      redirect_to questions_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @question = Question.find_by_id(params[:id])
+    if @question.destroy
+      flash[:success] = 'Question was destroyed!'
+    else
+      flash[:warning] = 'Error destroying poll...'
+    end
+    redirect_to questions_path
+  end
+
   private
 
   def question_params
-    params.require(:question).permit(:topic)
+    params.require(:question).permit(:topic, alternatives_attributes: [:id, :title, :_destroy])
   end
 end
