cec34049ee6d1515e3ab8c8ce3274cab8dc499d3
felipe <fe.valdiviaa@gmail.com>
2015-10-17 03:26:22 -0300

    Add relationship between alternative and votes models

--- app/models/alternative.rb
+++ app/models/alternative.rb
@@ -1,5 +1,8 @@
 class Alternative < ActiveRecord::Base
   belongs_to :question
 
+  has_many :votes, dependent: :destroy
+  has_many :users, through: :votes
+
   validates :title, presence: true
 end
