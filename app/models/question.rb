5484daa9337ac3f82462e72c829540072af06e67
felipe <fe.valdiviaa@gmail.com>
2015-10-17 03:26:50 -0300

    Add counter votes to questions

--- app/models/question.rb
+++ app/models/question.rb
@@ -5,4 +5,12 @@ class Question < ActiveRecord::Base
   validates :topic, presence: true
   # Accepts nested attribute for alternatives
   accepts_nested_attributes_for :alternatives, :reject_if => :all_blank, :allow_destroy => true
+
+  def normalized_votes_for(option)
+    votes_summary == 0 ? 0 : (option.votes.count.to_f / votes_summary) * 100
+  end
+
+  def votes_summary
+    alternatives.inject(0) {|summary, option| summary + option.votes.count}
+  end
 end
