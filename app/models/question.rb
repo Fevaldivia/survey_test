2d958c3ab0f333ec2d4bc4912c6d9c684378fcc2
felipe <fe.valdiviaa@gmail.com>
2015-10-17 00:32:51 -0300

    Accepts nested attributes for alternatives

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,4 +3,6 @@ class Question < ActiveRecord::Base
   has_many :alternatives, dependent: :destroy
   #validation
   validates :topic, presence: true
+  # Accepts nested attribute for alternatives
+  accepts_nested_attributes_for :alternatives, :reject_if => :all_blank, :allow_destroy => true
 end
