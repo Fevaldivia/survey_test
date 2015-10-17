f1e80d24d915bd88ca21d73a0076efdc0c4f1dba
felipe <fe.valdiviaa@gmail.com>
2015-10-17 03:28:58 -0300

    Add votes controller

---
+++ app/controllers/votes_controller.rb
@@ -0,0 +1,18 @@
+class VotesController < ApplicationController
+  def create
+    if current_user && params[:question] && params[:question][:id] && params[:alternative] && params[:alternative][:id]
+      @question = Question.find_by_id(params[:question][:id])
+      @option = @question.alternatives.find_by_id(params[:alternative][:id])
+      if @option && @question && !current_user.voted_for?(@question)
+        @option.votes.create({user_id: current_user.id})
+      else
+        render js: 'alert(\'Your vote cannot be saved. Have you already voted?\');'
+      end
+    else
+      render js: 'alert(\'Your vote cannot be saved.\');'
+    end
+
+    @option.votes.create({user_id: current_user.id})  + current_user.votes.create({alternative_id: @option.id})
+    current_user.alternatives.reset
+  end
+end
