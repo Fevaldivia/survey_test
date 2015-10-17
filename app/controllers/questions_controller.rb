a4bad76c3ffd41937af9a4b102151851730d3415
felipe <fe.valdiviaa@gmail.com>
2015-10-17 00:26:29 -0300

    Add questions controller

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,25 @@
+class QuestionsController < ApplicationController
+  def index
+    @questions = Question.all
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      flash[:success] = 'Question was created!'
+      redirect_to questions_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:topic)
+  end
+end
