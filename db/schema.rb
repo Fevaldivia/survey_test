da0ef5c40bac0bdaaa7e395dd347e5d8e2cb036c
felipe <fe.valdiviaa@gmail.com>
2015-10-17 01:40:09 -0300

    Add user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151017023230) do
+ActiveRecord::Schema.define(version: 20151017041634) do
 
   create_table "alternatives", force: :cascade do |t|
     t.string   "title"
@@ -27,5 +27,15 @@ ActiveRecord::Schema.define(version: 20151017023230) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "image_url"
+    t.string   "uid"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "users", ["uid"], name: "index_users_on_uid"
 
 end
